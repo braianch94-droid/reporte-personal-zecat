d386f5c70bb0605936b82ed28f4944074f17fe73
Braian Chevasco <bchevasco@zecat.com>
2026-05-19 09:33:19 -0300

    Actualizar.bat sube a GitHub Pages automaticamente

--- Actualizar.bat
+++ Actualizar.bat
@@ -20,8 +20,17 @@ if %ERRORLEVEL% NEQ 0 (
 )
 
 echo.
+echo Actualizando GitHub Pages...
+copy /Y "%~dp0Reporte_Personal.html" "%~dp0index.html" >nul
+cd /d "%~dp0"
+git add Reporte_Personal.html index.html
+git commit -m "Actualizar reporte %DATE% %TIME%"
+git push origin master
+
+echo.
 echo Abriendo reporte actualizado...
 start "" "%~dp0Reporte_Personal.html"
 
-echo Listo!
-timeout /t 2 /nobreak >nul
+echo.
+echo Listo! El reporte tambien se subio a GitHub Pages.
+timeout /t 3 /nobreak >nul
